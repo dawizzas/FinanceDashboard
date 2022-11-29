77fdccf548f5aa7ea9cbb1cc4df05350d47eec74
dawizzas <avels@uni.coventry.ac.uk>
2022-11-29 13:48:07 +0300

    Add/remove portfolio company

--- Server/Controllers/UserController.cs
+++ Server/Controllers/UserController.cs
@@ -1,12 +1,14 @@
 using System.Security.Claims;
 using FinanceDashboard.Server.Data;
 using FinanceDashboard.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinanceDashboard.Server.Controller;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
@@ -21,12 +23,16 @@ public class UserController : ControllerBase
     public async Task<ActionResult> UpdateCompanyPortfolio(Company CompanyToAdd)
     {
        try {
-           User currentUser = _context.Users.SingleOrDefault<User>(u => u.UserName == User.Identity.Name);
-           currentUser.PortfolioCompanies.Add(CompanyToAdd);
+           User currentUser = _context.Users.Include(u => u.PortfolioCompanies).SingleOrDefault<User>(u => u.UserName == User.Identity.Name);
 
+           if(currentUser.PortfolioCompanies.Where(c => c.Symbol.ToLower() == CompanyToAdd.Symbol.ToLower()).Count() > 0)
+                return NoContent();
+           
+           currentUser.PortfolioCompanies.Add(CompanyToAdd);
             _context.Entry(currentUser).State = EntityState.Modified;
             await _context.SaveChangesAsync();
             return NoContent();
+
        } catch(Exception e)
        {
            return Ok(e.Message);
@@ -37,8 +43,9 @@ public class UserController : ControllerBase
     public async Task<ActionResult> RemoveCompanyPortfolio(Company CompanyToRemove)
     {
        try {
-           User currentUser = _context.Users.SingleOrDefault<User>(u => u.UserName == User.Identity.Name);
-           currentUser.PortfolioCompanies.Remove(CompanyToRemove);
+           User currentUser = _context.Users.Include(u => u.PortfolioCompanies).SingleOrDefault<User>(u => u.UserName == User.Identity.Name);
+
+           currentUser.PortfolioCompanies.Remove(currentUser.PortfolioCompanies.Find(c => c.Symbol == CompanyToRemove.Symbol));
 
             _context.Entry(currentUser).State = EntityState.Modified;
             await _context.SaveChangesAsync();
@@ -47,5 +54,17 @@ public class UserController : ControllerBase
        {
            return Ok(e.Message);
        }
+    }
+
+    [HttpGet("getportfoliocompanies")]
+    public async Task<ActionResult<List<Company>>> GetAllPortfolioCompanies()
+    {
+        try {
+            User currentUser = _context.Users.Include(u => u.PortfolioCompanies).SingleOrDefault<User>(u => u.UserName == User.Identity.Name);
+            return Ok(currentUser.PortfolioCompanies);
+        } catch(Exception e)
+        {
+            return Ok(e.Message);
+        }
     }
 }
