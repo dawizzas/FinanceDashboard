6cfd450efcfda083f32f3d601be1580975b1aa71
dawizzas <avels@uni.coventry.ac.uk>
2023-01-01 07:11:27 +0300

    Company model updated

---
+++ Server/Migrations/20230101041023_ComapnyModelUpdated.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceDashboard.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComapnyModelUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "CollectiveBuyingPrice",
+                table: "Company",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Company",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CollectiveBuyingPrice",
+                table: "Company");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Company");
+        }
+    }
+}
